d97b8a21f019f049e653eed019d102a925c84903
David Hoese <david.hoese@ssec.wisc.edu>
2019-02-05 08:35:32 -0600

    Remove cibuildwheel version limit in CI environments

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -8,5 +8,5 @@ elif [[ "${BUILDMODE}" == "CIBUILDWHEEL" ]]; then
   if [[ $(uname) == "Darwin" ]]; then
     export PIP=pip2
   fi
-  $PIP install cibuildwheel==0.10.0
+  $PIP install cibuildwheel
 fi
