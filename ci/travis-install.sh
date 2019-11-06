ebf2425249ee189e8fd14dd15c6d87bf33712523
David Hoese <david.hoese@ssec.wisc.edu>
2019-11-06 13:57:41 -0600

    Switch travis back to upstream ci-helpers repository

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "${BUILDMODE}" == "ASTROPY" ]]; then
-#    git clone --depth 1 git://github.com/astropy/ci-helpers.git
-    git clone --depth 1 -b all-the-fixes git://github.com/djhoese/ci-helpers.git
+    git clone --depth 1 git://github.com/astropy/ci-helpers.git
     source ci-helpers/travis/setup_conda.sh
 elif [[ "${BUILDMODE}" == "CIBUILDWHEEL" ]]; then
   export PIP=pip
