4c7594d8dc072c3e09cacda241b6638213710150
David Hoese <david.hoese@ssec.wisc.edu>
2020-10-03 14:42:44 -0500

    Add debug messages to travis install for cron debug

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -2,7 +2,9 @@
 
 if [[ "${BUILDMODE}" == "ASTROPY" ]]; then
     git clone --depth 1 https://github.com/astropy/ci-helpers.git
+    echo "Done cloning ci-helpers"
     source ci-helpers/travis/setup_conda.sh
+    echo "Done sourcing conda environment"
 elif [[ "${BUILDMODE}" == "CIBUILDWHEEL" ]]; then
   export PIP=pip
   if [[ $(uname) == "Darwin" ]]; then
