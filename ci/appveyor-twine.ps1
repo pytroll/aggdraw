a21fe0725da761032a17dabc5098de26724b0d37
David Hoese <david.hoese@ssec.wisc.edu>
2022-01-25 20:54:55 -0600

    Copy CI changes from main

--- ci/appveyor-twine.ps1
+++
@@ -1,6 +0,0 @@
-# Small script for uploading wheels to pypi with twine
-
-if($env:appveyor_repo_tag -eq "true") {
-    python -m pip install twine
-    python -m twine upload --skip-existing dist/*.whl
-}
