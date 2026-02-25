56a1cda4e0781b0b5f970ca9caa928d4423f4c71
Austin Hurst <mynameisaustinhurst@gmail.com>
2026-02-25 00:32:08 -0400

    Fix API autodocs

--- aggdraw/__init__.py
+++ aggdraw/__init__.py
@@ -2,5 +2,7 @@ import aggdraw.aggdraw_cpp
 from .core import Draw, Pen, Brush, Path, Symbol
 from .dib import Dib
 
+__all__ = ["Pen", "Brush", "Path", "Symbol", "Draw", "Dib"]
+
 VERSION = aggdraw.aggdraw_cpp.VERSION
 __version__ = VERSION
